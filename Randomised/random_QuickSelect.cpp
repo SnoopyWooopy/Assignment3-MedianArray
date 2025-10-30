f4aaa0e6e003a14d32437204d323b2bba94d562b
Melisa <melisa.e.soria@student.uts.edu.au>
2025-10-30 11:37:28 +1100

    scaffolfing

--- Randomised/random_QuickSelect.cpp
+++ Randomised/random_QuickSelect.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 #include "random_QuickSelect.h"
+
+#include <vector>
+
+/*
+ Planning Random Quickselect --- To do this you must understand quicksort
+ Given array of n size (randomly given? unordered) find middle value (ordered)
+ mediam = n /2 (floor up)
+
+https://www.geeksforgeeks.org/dsa/quickselect-algorithm/
+ */
+
+//
+int partition(std::vector<int> & array) {
+
+}
+
+int quickSelect(std::vector<int> & array) {
+ // Find random pivot element
+ // Split array into less and greater
+
+}
