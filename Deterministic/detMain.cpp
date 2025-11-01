98a6243bb0796f22ce61b15cc93174a55f9e454b
Houria <houria.rauf@student.uts.edu.au>
2025-11-01 20:35:05 +1100

    median slice 2 - even lenght

--- Deterministic/detMain.cpp
+++ Deterministic/detMain.cpp
@@ -5,12 +5,18 @@
 #include <stdexcept>
 
 //medianSlice Tests
-TEST(findDeterministicTests, MedianSliceOddSize){ //to check if medianSlic correctl orders odd-length slice and returns median
+TEST(DeterministicTests, MedianSliceOddSize){ //to check if medianSlic correctl orders odd-length slice and returns median
     std::vector<int> arr = {5, 1, 9, 3, 7};
     EXPECT_EQ(medianSlice(arr, 0, 4), 5); // sorted {1,3,5,7,9}
 
 }
 
+TEST(DeterministicTests, MedianSliceEvenSize){ //to check if medianSlic correctly orders even-length slice and returns median
+    std::vector<int> arr = {4, 2, 8, 6};
+    EXPECT_EQ(medianSlice(arr, 0, 3), 6); // sorted {2, 4, 6, 8}, middle index = 2
+}
+
+//
 
 
 
