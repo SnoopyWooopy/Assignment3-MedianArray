7000fa3e76ce08889118ef8659507c22021847e6
Houria <houria.rauf@student.uts.edu.au>
2025-11-01 20:25:04 +1100

    #includes for the tests

--- Deterministic/detMain.cpp
+++ Deterministic/detMain.cpp
@@ -1,12 +1,5 @@
 // Student ID: 24749956 Houria Rauf.
+#include "Deterministic.h"
 #include <gtest/gtest.h>
-#include "../Deterministic/Determinitstic.h"
 #include <vector>
-#include <iostream>
-
-//testing the median slice
-TEST(DeterministicTests, MedianSliceBasic) { //does it correctly sort and return the correct median element
-    std::vector<int> arr = {5, 1, 9, 3, 7}; //make an array
-    int result = medianSlice(arr, 0, 4);
-    EXPECT_EQ(result, 5); // median of sorted {1,3,5,7,9}
-}
+#include <stdexcept>
