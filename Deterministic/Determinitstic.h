76e428e51c8fd211c884aee917363ebbda25c71b
Houria <houria.rauf@student.uts.edu.au>
2025-11-01 19:34:14 +1100

    add function declarations

--- Deterministic/Determinitstic.h
+++ Deterministic/Determinitstic.h
@@ -5,6 +5,16 @@
 #ifndef ASSIGNMENT3_DETERMINITSTIC_H
 #define ASSIGNMENT3_DETERMINITSTIC_H
 
+#include <vector>
+#include <stdexcept>
+
+int medianSlice(std::vector<int>& a, int l, int r);
+int findIndexSlice(const std::vector<int>& a, int l, int r, int val);
+int splitByPivot(std::vector<int>& a, int l, int r, int pivotVal);
+int findDeterministic(std::vector<int>& a, int l, int r, int k);
+int findMedianDeterministic(const std::vector<int>& input);
+
+
 
 
 
